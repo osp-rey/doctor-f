cedbcb8a21fecb83218b20b98e077f6e9ae96c68
pauchok-sk <pauchoooooook@gmail.com>
2026-07-14 13:41:24 +0300

    modal-dev

--- src/js/app.js
+++ src/js/app.js
@@ -6,4 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
   servicesDrop();
   burger();
   sliders();
+
+  Fancybox.bind("[data-fancybox]", {
+    closeButton: false,
+    on: {
+      destroy: (instance) => {
+        const id = instance.getSlide().src;
+
+        if (id.includes("#modal")) {
+          const modal = document.querySelector(id);
+          const inputNote = modal.querySelector(".input-note");
+          const modalTitle = modal.querySelector(".modal__title[data-text]");
+
+          if (inputNote) inputNote.value = "";
+          if (modalTitle) modalTitle.textContent = modalTitle.dataset.text;
+        }
+      },
+    },
+  });
+  Fancybox.show([{src: "#modal-feedback", type: "inline"}], {closeButton: false})
 });
--- src/js/functions/sliders.js
+++ src/js/functions/sliders.js
@@ -3,24 +3,63 @@ export default function sliders() {
 
   if (heroSlider) {
     const bgSlider = document.querySelector(".s-hero__bg-slider");
+    const sliderProgress = document.querySelector(".s-hero .slider-progress");
+    const slides = heroSlider.querySelectorAll(".swiper-slide");
 
     const swiperBg = new Swiper(bgSlider, {
       speed: 900,
       slidesPerView: 1,
-    })
+    });
 
     const swiper = new Swiper(heroSlider, {
       speed: 900,
       slidesPerView: 1,
-      // autoplay: {
-      //   delay: 7000
-      // },
+      autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: false,
+      },
       navigation: {
-
+        prevEl: ".s-hero .slider-arrow._prev",
+        nextEl: ".s-hero .slider-arrow._next",
       },
       controller: {
-        control: swiperBg
+        control: swiperBg,
+      },
+      on: {
+        init: ({ activeIndex, params }) => {
+          initSliderProgress(sliderProgress, slides.length, activeIndex);
+          changeSliderProgress(sliderProgress, activeIndex);
+        },
+        slideChangeTransitionEnd: ({ activeIndex, params }) => {
+          changeSliderProgress(sliderProgress, activeIndex);
+        },
+      },
+    });
+  }
+  function initSliderProgress(sliderProgress, count, activeIndex) {
+    for (let i = 0; i < count; i++) {
+      const item = document.createElement("span");
+      item.classList.add("slider-progress-item");
+
+      sliderProgress.appendChild(item);
+    }
+  }
+  function changeSliderProgress(sliderProgress, activeIndex) {
+    const items = sliderProgress.querySelectorAll(".slider-progress-item");
+    items.forEach((item, index) => {
+      if (index > activeIndex) {
+        item.style.animation = "none";
+        item.classList.remove("_active");
+        item.classList.remove("_fill");
+      } else if (index < activeIndex){
+        item.classList.remove("_active");
+        item.classList.add("_fill");
+      } else if (index === activeIndex) {
+        item.classList.remove("_fill");
       }
-    })
+    });
+
+    items[activeIndex].classList.add("_active");
   }
 }
